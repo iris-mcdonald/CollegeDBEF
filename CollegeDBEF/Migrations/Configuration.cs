c6a649a40c1ca2f196ba3aaef00a7372a6612132
iris-mcdonald <32845453+iris-mcdonald@users.noreply.github.com>
2018-03-12 13:22:14 -0400

    Add files via upload

--- CollegeDBEF/Migrations/Configuration.cs
+++ CollegeDBEF/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace CollegeDBEF.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CollegeDBEF.CollegeDBEF>
+    internal sealed class Configuration : DbMigrationsConfiguration<CollegeDBEF.CollegeDBEF1>
     {
-        public Configuration()
+        public  Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(CollegeDBEF.CollegeDBEF context)
+        protected override void Seed(CollegeDBEF.CollegeDBEF1 context)
         {
             //  This method will be called after migrating to the latest version.
 
